3abaed3330121566a30201503370622ef8ad8407
Vampy <kryptok112@gmail.com>
2019-10-19 16:19:20 +0800

    Initial cut of swap pool

---
+++ php/swap_pool.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Content-Type: application/json');
+error_reporting(0);
+
+$_POST['cc'] = 'dummy';
+require('get_json.php');
+
+if (!isset($_REQUEST['token']) || $_REQUEST['token'] != $app_password) die;
+
+$proxy_id = intval($_REQUEST["proxy"]);
+$proxy = $proxy_list[$proxy_id];
+$proxy_config_data = get_proxy_configs($proxy["ip"], $proxy["port"], $proxy["token"]);
+
+if($proxy_config_data && sizeof($proxy_config_data["pools"]) > 1) {
+    $prev_pool = $proxy_config_data["pools"][0];
+    unset($proxy_config_data["pools"][0]);
+    array_push($proxy_config_data["pools"], $prev_pool);
+    $proxy_config_data["pools"] = array_values($proxy_config_data["pools"]);
+    $response = write_config("http://".$proxy["ip"].":".$proxy["port"]."/1/config", $proxy_config_data, $proxy["token"]);
+}
+
+echo json_encode(true)."\n";
+?>
